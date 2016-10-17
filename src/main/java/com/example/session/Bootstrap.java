c7d951f89f3a605ee3e26ed396df2cef6d9280c5
btyamadasun <yamadasun@pm.nttdata.co.jp>
2016-10-17 15:33:41 +0900

    enable redis

--- src/main/java/com/example/session/Bootstrap.java
+++ src/main/java/com/example/session/Bootstrap.java
@@ -3,10 +3,14 @@ package com.example.session;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @ImportResource({ "classpath:/META-INF/spring/spring-security.xml",
 	"classpath:/META-INF/spring/applicationContext.xml", })
+@EnableRedisHttpSession
 public class Bootstrap extends SpringBootServletInitializer {
 
 	@Override
@@ -17,4 +21,8 @@ public class Bootstrap extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(Bootstrap.class, args);
 	}
+    @Bean
+    public JedisConnectionFactory connectionFactory() {
+            return new JedisConnectionFactory();
+    }
 }
